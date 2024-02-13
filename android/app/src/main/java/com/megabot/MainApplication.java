b30283ce58e09862bfe7df97ecccdd891e0b405f
KhurramAliKhan <khurram_khan123@hotmail.com>
2024-02-14 02:33:25 +0500

    fixes

--- android/app/src/main/java/com/megabot/MainApplication.java
+++ android/app/src/main/java/com/megabot/MainApplication.java
@@ -1,4 +1,4 @@
-package com.megabot;
+package com.megachatbot;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'Enter your server URL';
+export const BASE_URL = 'https://chatbot.dworkplace.com/';
 export const OPEN_AI_URL = 'https://api.openai.com/v1/';
