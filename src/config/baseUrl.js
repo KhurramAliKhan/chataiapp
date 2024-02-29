3d0a61f4c6bf41b7bff1f344b56c874270607c59
KhurramAliKhan <khurram_khan123@hotmail.com>
2024-02-29 23:41:03 +0500

    Update baseUrl.js

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://chatbot.dworkplace.com/api/';
+export const BASE_URL = 'https://chatbotai.nazsoft.com/api/';
 export const OPEN_AI_URL = 'https://api.openai.com/v1/';
